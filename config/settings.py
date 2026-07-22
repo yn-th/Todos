8a5d31ba101f8569073ca3f97e6e21d1f9bbc498
yn_th <yasin.thmaseby13@gmail.com>
2026-07-23 01:37:07 +0330

    update docker

--- config/settings.py
+++ config/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+from decouple import config, Csv
 
 
 # Quick-start development settings - unsuitable for production
