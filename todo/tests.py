8901e4b334641ec2f75bafa95401180db7d1fc0b
yn_th <yasin.thmaseby13@gmail.com>
2026-07-22 18:56:44 +0330

    add more test for API

--- todo/tests.py
+++ todo/tests.py
@@ -32,13 +32,13 @@ class TodoViewSetTest(APITestCase):
         self.todo2 = Todo.objects.create(
             name = "sara task",
             body = "this is second test",
-            assign_to = self.user1,
+            assign_to = self.user2,
             status = 'DN',
 
         ) 
 
-        self.list_url = reverse('todo_list_api')
-        self.detail_url = reverse('todo_detail_api',kwargs={'slug':self.todo1.slug})
+        self.list_url = reverse('todo-list')
+        self.detail_url = reverse('todo-detail',kwargs={'slug':self.todo1.slug})
 
     def test_guest_cannot_access_list(self):
         response = self.client.get(self.list_url)
@@ -55,3 +55,26 @@ class TodoViewSetTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data['results']), 1)
         self.assertEqual(response.data['results'][0]['name'], self.todo1.name)
+
+    def test_create_task_sets_assignee(self):
+        self.client.force_authenticate(user = self.user1)
+        data = {
+            'name':'new task',
+            'body':'this is new task for ali',
+            'priority':'H',
+            'status':'SE',
+            }
+        response = self.client.post(self.list_url,data,format='json')
+        self.assertEqual(response.status_code,status.HTTP_201_CREATED)
+        self.assertEqual(response.data['assign_to'],self.user1)
+    
+    def test_cannot_edit_others_task(self):
+        self.client.force_authenticate(user=self.user2)
+        response = self.client.put(self.detail_url, {'name': 'هک شده'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_owner_can_delete(self):
+        self.client.force_authenticate(user=self.user1)
+        response = self.client.delete(self.detail_url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Todo.objects.filter(slug=self.todo1.slug).exists())
